4cf90119b7ee5cc9da063af4cf3c1b4cc2ae449e
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-05-07 22:28:20 +0200

    added processingInQueue

--- server/src/websockets.js
+++ server/src/websockets.js
@@ -1,4 +1,5 @@
-const { addConnecton, removeConnection, getConnectionList, myPosition } = require("./queue");
+const { getLatest } = require("./files");
+const { addConnecton, removeConnection, getConnectionList, myPosition, getFirst, getProcessingCurrently, setProcessingCurrently } = require("./queue");
 
 function onConnect(io, socket) {
     const id = socket.id;
@@ -11,11 +12,12 @@ function onConnect(io, socket) {
     }
 
     sendPositionToClients(io);
-
+    processinQueue(io);
 
     socket.on("disconnect", (reason) => {
         removeConnection(id);
         sendPositionToClients(io);
+        processinQueue(io);
     });
     // Add the client to the queue and broadcast an update on how many users are in the QUEQUE and what is your position
 }
@@ -25,24 +27,36 @@ Send the position in the queue to the socket ids
 */
 function sendPositionToClients(io) {
     const connectionList = getConnectionList();
+    const currentlyProcessing = getProcessingCurrently();
     for (let i = 0; i < connectionList.length; i++) {
         const socketId = connectionList[i];
-        const pos = myPosition(socketId);
-        const length = connectionList.length;
-        io.to(socketId).emit(messageRouter(MessageRoutes.position, { pos, length }));
 
+        if (currentlyProcessing !== socketId) {
+            const pos = myPosition(socketId);
+            const length = connectionList.length;
+            io.to(socketId).emit(messageRouter(MessageRoutes.position, { pos, length }));
+        }
     }
 }
 
-function processinQueue() {
-    // I get the first one from the queue and send a message with the file's location
-    // They can start processing the file
+function processinQueue(io) {
+    const firstInQueue = getFirst();
 
+    const currentlyProcessing = getProcessingCurrently();
+
+    // if the current connection is processing the contribution already do nothing!
+    if (firstInQueue === currentlyProcessing) {
+        return;
+    }
+
+    setProcessingCurrently(firstInQueue);
+    getLatest((filename) => io.to(firstInQueue).emit(messageRouter(MessageRoutes.startContribution, { filename })));
 }
 
 const MessageRoutes = {
     position: "position",
-    closeConnection: "closeConnection"
+    closeConnection: "closeConnection",
+    startContribution: "startContribution"
 }
 
 function messageRouter(type, msg) {
